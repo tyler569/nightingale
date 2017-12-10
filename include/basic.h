6ce562bf3405a194f504fb7b09b954b370f872d5
Tyler Philbrick <t5j6p9@gmail.com>
2017-12-10 13:38:12 -0800

    Minor changes to basic

--- include/basic.h
+++ include/basic.h
@@ -3,18 +3,19 @@
 #ifndef NIGHTINGALE_BASIC_H
 #define NIGHTINGALE_BASIC_H
 
-_Static_assert(__STDC_VERSION__ >= 201112L, "Nightingale must be compiled as C11 or greater");
-#ifndef HOSTED
-_Static_assert(__STDC_HOSTED__ != 1, "Nightingale must not be compiled in a hosted environment");
-#endif
+#define static_assert _Static_assert
 
-// Integer types
+static_assert(__STDC_VERSION__ >= 201112L, "Nightingale must be compiled as C11 or greater");
+static_assert(__STDC_HOSTED__ != 1, "Nightingale must not be compiled in a hosted environment");
 
-_Static_assert(__CHAR_BIT__ == 8, "Bytes must be 8 bits (How did you even do this?)");
-_Static_assert(sizeof(short int) == 2, "Short must be 2 bytes");
-_Static_assert(sizeof(int) == 4, "Int must be 4 bytes");
-_Static_assert(sizeof(long int) == 8, "Long must be 8 bytes");
-_Static_assert(sizeof(void *) == 8, "Pointer must be 8 bytes (Are you using a 32 bit compiler?)");
+// Numeric types
+#define NO_FLOAT
+
+static_assert(__CHAR_BIT__ == 8, "Bytes must be 8 bits (How did you even do this?)");
+static_assert(sizeof(short int) == 2, "Short must be 2 bytes");
+static_assert(sizeof(int) == 4, "Int must be 4 bytes");
+static_assert(sizeof(long int) == 8, "Long must be 8 bytes");
+static_assert(sizeof(void *) == 8, "Pointer must be 8 bytes (Are you using a 32 bit compiler?)");
 
 typedef unsigned char u8;
 typedef unsigned short int u16;
@@ -29,6 +30,11 @@ typedef signed int i32;
 typedef signed long int i64;
 typedef signed long int isize;
 typedef __int128_t i128;
+
+#ifndef NO_FLOAT
+typedef float f32
+typedef double f64
+#endif
 
 // Boolean type
 
