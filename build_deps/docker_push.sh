a6241bdc56cb7a6a158d646488abffb7efe8dc2a
ConnorJarvis <connorpjarvis@gmail.com>
2019-04-11 19:25:54 -0400

    Added 32bit builds

--- build_deps/docker_push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-$(git diff --name-only ${TRAVIS_COMMIT}^ ${TRAVIS_COMMIT} | grep build_deps/Dockerfile > /dev/null)
-dockerfile_status=$?
-
-if [[ ${dockerfile_status} == "0" ]]
-then
-    eval "docker push tyler569/nightingale-build"
-fi
