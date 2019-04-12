2f1c56a6b1dcbe9d4ca7acc8a9a361be9d9fefb6
Tyler Philbrick <t5j6p9@gmail.com>
2019-04-11 18:25:11 -0700

    elf loader: refactor logic
    I want to make gradual improvements to the older parts of the kernel,
    and one of those major things I want to do is factor logic out into sub
    functions for ease of understanding.  This commit starts this
    work for the ELF loader.

--- kernel/elf.c
+++ kernel/elf.c
@@ -80,6 +80,30 @@ int elf_verify(void* elf) {
     }
 }
 
+
+void init_section(void* dst_vaddr, size_t len) {
+    uintptr_t bot = round_down((uintptr_t)dst_vaddr, 0x1000);
+    uintptr_t top = round_up((uintptr_t)dst_vaddr + len, 0x1000);
+
+    for (uintptr_t p = bot; p<=top; p += 0x1000)
+        vmm_create_unbacked(p, PAGE_USERMODE | PAGE_WRITEABLE);
+}
+
+void load_section(void* dst_vaddr, void* src_vaddr, size_t flen, size_t mlen) {
+    memcpy(dst_vaddr, src_vaddr, flen);
+
+    // BSS is specified by having p_memsz > p_filesz
+    // you are expected to zero the extra space
+    if (mlen > flen) {
+        memset((char*)dst_vaddr + flen, 0, mlen - flen);
+    }
+}
+
+void* elf_at(void* elf, size_t offset) {
+    return ((char*)elf) + offset;
+}
+
+
 int elf_load(void* elf_) {
     int bits = elf_verify(elf_);
     if (bits == 0) {
@@ -92,21 +116,13 @@ int elf_load(void* elf_) {
         Elf64_Phdr* phdr = (Elf64_Phdr*)phdr_l;
 
         for (int i=0; i<elf->e_phnum; i++) {
-            if (phdr[i].p_type != PT_LOAD)
+            Elf64_Phdr* sec = &phdr[i];
+            if (sec->p_type != PT_LOAD)
                 continue;
+            void* section = elf_at(elf, sec->p_offset);
 
-            uintptr_t page = phdr[i].p_vaddr & PAGE_MASK_4K;
-            for (size_t off = 0; off <= phdr[i].p_memsz + 0x1000; off += 0x1000) {
-                // if the pages already exist, they are recycled, since creating an
-                // existing page is a noop and COW forks are a thing
-                vmm_create_unbacked(page + off, PAGE_USERMODE | PAGE_WRITEABLE);
-            }
-
-            memcpy((char*)phdr[i].p_vaddr, ((char*)elf) + phdr[i].p_offset, phdr[i].p_filesz);
-            if (phdr[i].p_memsz > phdr[i].p_filesz) {
-                // printf("zeroing some things (probably in the bss");
-                memset(((char*)phdr[i].p_vaddr) + phdr[i].p_filesz, 0, phdr[i].p_memsz - phdr[i].p_filesz);
-            }
+            init_section((void*)sec->p_vaddr, sec->p_memsz);
+            load_section((void*)sec->p_vaddr, section, sec->p_filesz, sec->p_memsz);
         }
         return 0;
     } else if (bits == 32) {
@@ -115,20 +131,13 @@ int elf_load(void* elf_) {
         Elf32_Phdr* phdr = (Elf32_Phdr*)phdr_l;
 
         for (int i=0; i<elf->e_phnum; i++) {
-            if (phdr[i].p_type != PT_LOAD)
+            Elf32_Phdr* sec = &phdr[i];
+            if (sec->p_type != PT_LOAD)
                 continue;
+            void* section = elf_at(elf, sec->p_offset);
 
-            uintptr_t page = phdr[i].p_vaddr & PAGE_MASK_4K;
-            for (size_t off = 0; off <= phdr[i].p_memsz; off += 0x1000) {
-                vmm_create_unbacked(page + off, PAGE_USERMODE | PAGE_WRITEABLE);
-                // if the pages already exist, they are recycled, since creating an
-                // existing page is a noop and COW forks are a thing
-            }
-
-            if (phdr[i].p_memsz > phdr[i].p_filesz) {
-                // printf("zeroing some things (probably in the bss");
-                memset(((char*)phdr[i].p_vaddr) + phdr[i].p_filesz, 0, phdr[i].p_memsz - phdr[i].p_filesz);
-            }
+            init_section((void*)sec->p_vaddr, sec->p_memsz);
+            load_section((void*)sec->p_vaddr, section, sec->p_filesz, sec->p_memsz);
         }
         return 0;
     } else {
