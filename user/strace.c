feebbc7c0e7b7ef063b06f32538bd693d6448368
Tyler Philbrick <t5j6p9@gmail.com>
2019-10-04 00:54:08 -0700

    strace: simplify return logic
    This was just nedlessly complicated for what I was doing. Just return
    the returned status.

--- user/strace.c
+++ user/strace.c
@@ -3,10 +3,5 @@
 
 int main(int argc, char **argv) {
         strace(1);
-        int success = execve(argv[1], argv + 2, NULL);
-        if (success) {
-                // perror("execve()");
-                return 1;
-        }
-        return 0;
+        return execve(argv[1], argv + 2, NULL);
 }
