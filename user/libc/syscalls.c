78b392079ff118c34fc16449cde2a536aaabafd3
Tyler Philbrick <t5j6p9@gmail.com>
2019-10-13 12:16:01 -0700

    syscalls: rename syscalls to NG_
    The main motivation for this is to distinguish them better as I add
    support for more kernels to the libc. I will also change the
    libc/syscalls.c wrapper functions to match (`read` -> `ng_read`) to
    improve the porting experience.
    
    i.e.:
    ```
    long read(int fd, char *buf, long len) {
    \#if __nightingale__
        return ng_read(fd, buf, len);
    \#elif __linux__
        // do something kernel-specific
        return linux_read(fd, buf, len);
    \#endif
    }
    ```

--- user/libc/syscalls.c
+++ user/libc/syscalls.c
@@ -13,12 +13,12 @@
 #include "syscall.h"
 
 noreturn void exit(int status) {
-        syscall1(SYS_EXIT, status);
+        syscall1(NG_EXIT, status);
         __builtin_unreachable();
 }
 
 noreturn void exit_group(int status) {
-        syscall1(SYS_EXIT_GROUP, status);
+        syscall1(NG_EXIT_GROUP, status);
         __builtin_unreachable();
 }
 
@@ -31,68 +31,68 @@ noreturn void exit_group(int status) {
 
 ssize_t read(int fd, void *data, size_t len) {
         struct syscall_ret ret =
-            syscall3(SYS_READ, (intptr_t)fd, (intptr_t)data, (intptr_t)len);
+            syscall3(NG_READ, (intptr_t)fd, (intptr_t)data, (intptr_t)len);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 ssize_t write(int fd, const void *data, size_t len) {
         struct syscall_ret ret =
-            syscall3(SYS_WRITE, (intptr_t)fd, (intptr_t)data, (intptr_t)len);
+            syscall3(NG_WRITE, (intptr_t)fd, (intptr_t)data, (intptr_t)len);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 pid_t fork(void) {
-        struct syscall_ret ret = syscall0(SYS_FORK);
+        struct syscall_ret ret = syscall0(NG_FORK);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 pid_t getpid(void) {
-        struct syscall_ret ret = syscall0(SYS_GETPID);
+        struct syscall_ret ret = syscall0(NG_GETPID);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 pid_t gettid(void) {
-        struct syscall_ret ret = syscall0(SYS_GETTID);
+        struct syscall_ret ret = syscall0(NG_GETTID);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 int execve(char *program, char **argv, char **envp) {
-        struct syscall_ret ret = syscall3(SYS_EXECVE, (intptr_t)program,
+        struct syscall_ret ret = syscall3(NG_EXECVE, (intptr_t)program,
                                           (intptr_t)argv, (intptr_t)envp);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 // deprecated pending rework - see kernel/thread.c:sys_wait4
 // int wait4(pid_t pid) {
-//     syscall1(SYS_WAIT4, (intptr_t)pid);
+//     syscall1(NG_WAIT4, (intptr_t)pid);
 // }
 
 int socket(int domain, int type, int protocol) {
-        struct syscall_ret ret = syscall3(SYS_SOCKET, (intptr_t)domain,
+        struct syscall_ret ret = syscall3(NG_SOCKET, (intptr_t)domain,
                                           (intptr_t)type, (intptr_t)protocol);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 int strace(bool enable) {
-        struct syscall_ret ret = syscall1(SYS_STRACE, (intptr_t)enable);
+        struct syscall_ret ret = syscall1(NG_STRACE, (intptr_t)enable);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 int connect(int sock, const struct sockaddr *addr, socklen_t addrlen) {
-        struct syscall_ret ret = syscall3(SYS_CONNECT, (intptr_t)sock,
+        struct syscall_ret ret = syscall3(NG_CONNECT, (intptr_t)sock,
                                           (intptr_t)addr, (intptr_t)addrlen);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen) {
-        struct syscall_ret ret = syscall3(SYS_BIND, (intptr_t)sockfd,
+        struct syscall_ret ret = syscall3(NG_BIND, (intptr_t)sockfd,
                                           (intptr_t)addr, (intptr_t)addrlen);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 ssize_t send(int sock, const void *buf, size_t len, int flags) {
         struct syscall_ret ret =
-            syscall4(SYS_SEND, (intptr_t)sock, (intptr_t)buf, (intptr_t)len,
+            syscall4(NG_SEND, (intptr_t)sock, (intptr_t)buf, (intptr_t)len,
                      (intptr_t)flags);
         RETURN_OR_SET_ERRNO(ret);
 }
@@ -100,14 +100,14 @@ ssize_t send(int sock, const void *buf, size_t len, int flags) {
 ssize_t sendto(int sock, const void *buf, size_t len, int flags,
                const struct sockaddr *remote, socklen_t addrlen) {
         struct syscall_ret ret = syscall6(
-            SYS_SENDTO, (intptr_t)sock, (intptr_t)buf, (intptr_t)len,
+            NG_SENDTO, (intptr_t)sock, (intptr_t)buf, (intptr_t)len,
             (intptr_t)flags, (intptr_t)remote, (intptr_t)addrlen);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 ssize_t recv(int sock, void *buf, size_t len, int flags) {
         struct syscall_ret ret =
-            syscall4(SYS_RECV, (intptr_t)sock, (intptr_t)buf, (intptr_t)len,
+            syscall4(NG_RECV, (intptr_t)sock, (intptr_t)buf, (intptr_t)len,
                      (intptr_t)flags);
         RETURN_OR_SET_ERRNO(ret);
 }
@@ -115,54 +115,54 @@ ssize_t recv(int sock, void *buf, size_t len, int flags) {
 ssize_t recvfrom(int sock, void *buf, size_t len, int flags,
                  struct sockaddr *remote, socklen_t *addrlen) {
         struct syscall_ret ret = syscall6(
-            SYS_RECVFROM, (intptr_t)sock, (intptr_t)buf, (intptr_t)len,
+            NG_RECVFROM, (intptr_t)sock, (intptr_t)buf, (intptr_t)len,
             (intptr_t)flags, (intptr_t)remote, (intptr_t)addrlen);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 int waitpid(pid_t pid, int *status, int options) {
         struct syscall_ret ret = syscall3(
-            SYS_WAITPID, (intptr_t)pid, (intptr_t)status, (intptr_t)options);
+            NG_WAITPID, (intptr_t)pid, (intptr_t)status, (intptr_t)options);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 int dup2(int oldfd, int newfd) {
         struct syscall_ret ret =
-            syscall2(SYS_DUP2, (intptr_t)oldfd, (intptr_t)newfd);
+            syscall2(NG_DUP2, (intptr_t)oldfd, (intptr_t)newfd);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 int uname(struct utsname *name) {
-        struct syscall_ret ret = syscall1(SYS_UNAME, (intptr_t)name);
+        struct syscall_ret ret = syscall1(NG_UNAME, (intptr_t)name);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 int yield(void) {
-        struct syscall_ret ret = syscall0(SYS_YIELD);
+        struct syscall_ret ret = syscall0(NG_YIELD);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 int open(const char *name, int flags) {
         struct syscall_ret ret =
-            syscall2(SYS_OPEN, (intptr_t)name, (intptr_t)flags);
+            syscall2(NG_OPEN, (intptr_t)name, (intptr_t)flags);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 int seek(int fd, off_t offset, int whence) {
-        struct syscall_ret ret = syscall3(SYS_SEEK, (intptr_t)fd,
+        struct syscall_ret ret = syscall3(NG_SEEK, (intptr_t)fd,
                                           (intptr_t)offset, (intptr_t)whence);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 int poll(struct pollfd *pollfds, nfds_t nfds, int timeout) {
-        struct syscall_ret ret = syscall3(SYS_POLL, (intptr_t)pollfds,
+        struct syscall_ret ret = syscall3(NG_POLL, (intptr_t)pollfds,
                                           (intptr_t)nfds, (intptr_t)timeout);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 void *mmap(void *addr, size_t len, int prot, int flags, int fd, off_t off) {
         struct syscall_ret ret =
-            syscall6(SYS_MMAP, (intptr_t)addr, (intptr_t)len, (intptr_t)prot,
+            syscall6(NG_MMAP, (intptr_t)addr, (intptr_t)len, (intptr_t)prot,
                      (intptr_t)flags, (intptr_t)fd, (intptr_t)off);
         if (ret.is_error) {
                 errno = ret.value;
@@ -173,50 +173,50 @@ void *mmap(void *addr, size_t len, int prot, int flags, int fd, off_t off) {
 
 int munmap(void *addr, size_t len) {
         struct syscall_ret ret =
-            syscall2(SYS_MUNMAP, (intptr_t)addr, (intptr_t)len);
+            syscall2(NG_MUNMAP, (intptr_t)addr, (intptr_t)len);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 int heapdbg(int type) {
-        struct syscall_ret ret = syscall1(SYS_HEAPDBG, (intptr_t)type);
+        struct syscall_ret ret = syscall1(NG_HEAPDBG, (intptr_t)type);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 int setpgid(void) {
-        struct syscall_ret ret = syscall0(SYS_SETPGID);
+        struct syscall_ret ret = syscall0(NG_SETPGID);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 int top(int show_threads) {
-        struct syscall_ret ret = syscall1(SYS_TOP, show_threads);
+        struct syscall_ret ret = syscall1(NG_TOP, show_threads);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 int clone(int (*fn)(void *), void *arg, void *new_stack, int flags) {
         struct syscall_ret ret =
-            syscall4(SYS_CLONE0, (intptr_t)fn, (intptr_t)new_stack,
+            syscall4(NG_CLONE0, (intptr_t)fn, (intptr_t)new_stack,
                      (intptr_t)arg, flags);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 int load_module(int fd) {
-        struct syscall_ret ret = syscall1(SYS_LOADMOD, fd);
+        struct syscall_ret ret = syscall1(NG_LOADMOD, fd);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 noreturn int haltvm(int exit_code) {
-        struct syscall_ret ret = syscall1(SYS_HALTVM, exit_code);
+        struct syscall_ret ret = syscall1(NG_HALTVM, exit_code);
         __builtin_unreachable();
 }
 
 int openat(int fd, const char *name, int flags) {
         struct syscall_ret ret =
-            syscall3(SYS_OPENAT, fd, (intptr_t)name, (intptr_t)flags);
+            syscall3(NG_OPENAT, fd, (intptr_t)name, (intptr_t)flags);
         RETURN_OR_SET_ERRNO(ret);
 }
 
 int execveat(int fd, char *program, char **argv, char **envp) {
-        struct syscall_ret ret = syscall4(SYS_EXECVEAT, fd, (intptr_t)program,
+        struct syscall_ret ret = syscall4(NG_EXECVEAT, fd, (intptr_t)program,
                                           (intptr_t)argv, (intptr_t)envp);
         RETURN_OR_SET_ERRNO(ret);
 }
