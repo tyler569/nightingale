d9efcb85e8544bed4748ef041f88e655c0e59cac
Tyler Philbrick <t5j6p9@gmail.com>
2019-10-28 19:15:34 -0700

    user: c++ support
    This introduces simple and experimental support for C++ in usermode
    programs. There is no C++ standard library. There are probably no global
    object constructors called. This may not even stick around, but it's fun
    to be able to say that it works and it introduces almost no additional
    work since gcc can already compile C++.

---
+++ user/cxx.cc
@@ -0,0 +1,29 @@
+
+extern "C" {
+#include <stdio.h>
+}
+
+template <typename T>
+class sample_class {
+public:
+        T value;
+
+        sample_class() {
+                printf("Constructing a %s\n", __PRETTY_FUNCTION__);
+        }
+
+        ~sample_class() {
+                printf("Destructing a %s\n", __PRETTY_FUNCTION__);
+        }
+};
+
+extern "C"
+int main() {
+        sample_class<int> object{};
+
+        printf("object value is %i\n", object.value);
+
+        return 0;
+}
+
+
