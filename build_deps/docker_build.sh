b0a831ec1947a9bbe4030981be22758c716ab353
Tyler Philbrick <t5j6p9@gmail.com>
2019-10-30 23:45:17 -0700

    travis build: add lua to matrix

--- build_deps/docker_build.sh
+++ build_deps/docker_build.sh
@@ -8,15 +8,20 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 fi
 
 
-eval "docker pull ${DOCKER_USERNAME}/nightingale-build-${ARCHLOWER}:latest"
+docker pull ${DOCKER_USERNAME}/nightingale-build-${ARCHLOWER}:latest
 
-eval "docker build -t ${DOCKER_USERNAME}/nightingale-build-${ARCHLOWER} --cache-from ${DOCKER_USERNAME}/nightingale-build-${ARCHLOWER}:latest build_deps/${ARCH}/"
+docker build -t ${DOCKER_USERNAME}/nightingale-build-${ARCHLOWER} \
+        --cache-from ${DOCKER_USERNAME}/nightingale-build-${ARCHLOWER}:latest \
+        build_deps/${ARCH}/
 
 
 if [[ ${TRAVIS_SECURE_ENV_VARS} == "true" ]]
 then
-eval "docker push ${DOCKER_USERNAME}/nightingale-build-${ARCHLOWER}"
+docker push ${DOCKER_USERNAME}/nightingale-build-${ARCHLOWER}
 fi
 
-eval "docker run --rm -v "${TRAVIS_BUILD_DIR}":/nightingale ${DOCKER_USERNAME}/nightingale-build-${ARCHLOWER} /bin/sh -c \"cd /nightingale && make ARCH=$ARCH\""
+docker run --rm -v "${TRAVIS_BUILD_DIR}":/nightingale \
+        --env ARCH --env BUILD_LUA \
+        ${DOCKER_USERNAME}/nightingale-build-${ARCHLOWER} \
+        /bin/sh -c "cd /nightingale && make"
 
