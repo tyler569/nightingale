53a7678a2a4a26efa65a4eca7bab5570ad69eb95
Tyler Philbrick <t5j6p9@gmail.com>
2019-11-12 00:00:37 -0800

    user/write: check failure

--- user/write.c
+++ user/write.c
@@ -1,9 +1,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 int main(int argc, char **argv) {
         FILE *s = fopen("foobar", "w");
+        if (!s) {
+                perror("fopen()");
+                return EXIT_FAILURE;
+        }
         for (int i=0; i<argc; i++) {
                 fprintf(s, "%s\n", argv[i]);
         }
