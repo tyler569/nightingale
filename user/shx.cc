179370b56837de0509fc65805aef4dae8ba14539
Tyler Philbrick <t5j6p9@gmail.com>
2019-11-23 08:50:36 -0800

    user/shx: add shx program (a C++ shell) (to-be)

---
+++ user/shx.cc
@@ -0,0 +1,16 @@
+
+#include <basic.h>
+#include <memory.hh>
+#include <iostream.hh>
+#include <string.hh>
+
+using nx::string;
+using nx::cout;
+
+int main() {
+        string foo = "Hello world!\n";
+        cout << foo;
+
+        return 0;
+}
+
