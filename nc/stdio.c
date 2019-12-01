0349f63415b770ddea9ae287bd34c18addf7d082
Tyler Philbrick <t5j6p9@gmail.com>
2019-12-01 12:09:02 -0700

    nc/stdio: output terminal \0 in non-truncated *nprintf

--- nc/stdio.c
+++ nc/stdio.c
@@ -491,7 +491,7 @@ int vsnprintf(char *buf, size_t len, const char *format, va_list args) {
         char internal[512];
         int internal_len = vsprintf(internal, format, args);
 
-        int written = min(len, internal_len);
+        int written = min(len, internal_len+1);
         memcpy(buf, internal, written);
         return internal_len;
 }
