d85ce053127da0316e6ec52f3998cbbceedd76bb
Tyler Philbrick <t5j6p9@gmail.com>
2020-04-19 14:10:02 -0700

    libc: add license attribution in crt0

--- libc/entry.c
+++ libc/entry.c
@@ -9,6 +9,7 @@ int main(int, char **);
 
 void initialize_standard_library() {
         malloc_initialize(NULL, (16 * 1024*1024));
+        // printf("stdlib init\n");
 }
 
 int nc_start(int argc, char **argv, char **envp) {
