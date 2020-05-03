c702d9c095ff76d4b658c86b0ba4fa2426325a14
Tyler Philbrick <t5j6p9@gmail.com>
2020-05-02 18:43:34 -0700

    kill: beef up kill to support different signals
    a la `kill -CHLD 1`

--- user/kill.c
+++ user/kill.c
@@ -1,17 +1,78 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include <sys/types.h>
 #include <signal.h>
 
+char *signal_shortnames[] = {
+        [SIGABRT]   = "ABRT",
+        [SIGALRM]   = "ALRM",
+        [SIGBUS]    = "BUS",
+        [SIGCHLD]   = "CHLD",
+        [SIGCONT]   = "CONT",
+        [SIGFPE]    = "FPE",
+        [SIGHUP]    = "HUP",
+        [SIGILL]    = "ILL",
+        [SIGINFO]   = "INFO",
+        [SIGINT]    = "INT",
+        [SIGKILL]   = "KILL",
+        [SIGPIPE]   = "PIPE",
+        [SIGPROF]   = "PROF",
+        [SIGQUIT]   = "QUIT",
+        [SIGSEGV]   = "SEGV",
+        [SIGSTOP]   = "STOP",
+        [SIGTSTP]   = "TSTP",
+        [SIGSYS]    = "SYS",
+        [SIGTERM]   = "TERM",
+        [SIGTRAP]   = "TRAP",
+        [SIGTTIN]   = "TTIN",
+        [SIGTTOU]   = "TTOU",
+        [SIGURG]    = "URG",
+        [SIGUSR1]   = "USR1",
+        [SIGUSR2]   = "USR2",
+        [SIGVTALRM] = "VTALRM",
+        [SIGXCPU]   = "XCPU",
+        [SIGWINCH]  = "WINCH",
+};
+
+int signal_by_name(char *name) {
+        int signal = -1;
+        for (int i=0; i<SIGWINCH+1; i++) {
+                if (strcmp(signal_shortnames[i], name) == 0) {
+                        signal = i;
+                        break;
+                }
+        }
+        return signal;
+}
+
 int main(int argc, char **argv) {
         if (argc < 2) {
-                printf("Not enough arguments\n");
-                exit(1);
+                fprintf(stderr, "kill: not enough arguments\n");
+                return EXIT_FAILURE;
+        }
+
+        int signal = SIGINT;
+
+        if (argv[1][0] == '-') {
+                signal = signal_by_name(&argv[1][1]);
+                if (signal == -1) {
+                        fprintf(stderr, "%s is not a valid signal.\n", &argv[0][1]);
+                        return EXIT_FAILURE;
+                }
+                argv += 1;
+        }
+
+        if (!argv[1]) {
+                fprintf(stderr, "kill: not enough arguments\n");
+                return EXIT_FAILURE;
         }
 
         pid_t pid = atoi(argv[1]);
-        kill(pid, SIGINT);
-        return EXIT_SUCCESS;
+        kill(pid, signal);
+
+        return errno == SUCCESS;
 }
 
