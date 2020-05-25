a5c311601376996fbbe8e13e7a63b163cf29eb0e
Tyler Philbrick <t5j6p9@gmail.com>
2020-05-25 14:13:49 -0700

    WIP net: remove the old c files, will rebuild

--- net/include/arpa/inet.h
+++ net/include/arpa/inet.h
@@ -19,3 +19,20 @@ static inline uint16_t htons(uint16_t x) {
         return (((x & 0xFF00) >> 8) | ((x & 0x00FF) << 8));
 }
 
+static inline uint32_t ntohl(uint32_t x) {
+        return (
+                ((x & 0xFF000000) >> 24) |
+                ((x & 0x00FF0000) >> 8)  |
+                ((x & 0x0000FF00) << 8)  |
+                ((x & 0x000000FF) << 24)
+        );
+}
+
+static inline uint32_t htonl(uint32_t x) {
+        return (
+                ((x & 0xFF000000) >> 24) |
+                ((x & 0x00FF0000) >> 8)  |
+                ((x & 0x0000FF00) << 8)  |
+                ((x & 0x000000FF) << 24)
+        );
+}
