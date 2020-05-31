ec29e151bc5c78ee9ed854e1665a45ef160b5724
Tyler Philbrick <t5j6p9@gmail.com>
2020-05-30 20:16:42 -0700

    net: add net back to sysroot.sh

--- sysroot.sh
+++ sysroot.sh
@@ -7,5 +7,6 @@ mkdir -p sysroot/usr/share
 cp -LRT include sysroot/usr/include
 cp -LRT libc/include sysroot/usr/include
 cp -LRT kernel/include sysroot/usr/include
+cp -LRT net/include sysroot/usr/include
 cp -LRT linker/include sysroot/usr/include
 cp -LRT external/libm/include sysroot/usr/include
