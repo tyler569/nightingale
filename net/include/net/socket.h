cffc9df0dfc258e37ac0eaf6e5d74a583e16ca54
Tyler Philbrick <t5j6p9@gmail.com>
2020-05-31 18:39:21 -0700

    net/socket: idk some socket crap

--- net/include/net/socket.h
+++ net/include/net/socket.h
@@ -8,73 +8,75 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <net/core.h>
+#include <list.h>
 
 enum socket_state {
-    SOCKET_IDLE = 0,
-    SOCKET_REQUESTED,
-    SOCKET_BOUND,
-    SOCKET_LISTENING,
-    SOCKET_OUTBOUND,
+        SOCKET_IDLE = 0,
+        SOCKET_REQUESTED,
+        SOCKET_BOUND,
+        SOCKET_LISTENING,
+        SOCKET_OUTBOUND,
 };
 
 enum tcp_state {
-    TCP_S_LISTEN,
-    TCP_S_SYN_SENT,
-    TCP_S_SYN_RECIEVED,
-    TCP_S_ESTABLISHED,
-    TCP_S_FIN_WAIT_1,
-    TCP_S_FIN_WAIT_2,
-    TCP_S_CLOSE_WAIT,
-    TCP_S_CLOSING,
-    TCP_S_LAST_ACK,
-    TCP_S_TIME_WAIT,
-    TCP_S_CLOSED,
+        TCP_S_LISTEN,
+        TCP_S_SYN_SENT,
+        TCP_S_SYN_RECIEVED,
+        TCP_S_ESTABLISHED,
+        TCP_S_FIN_WAIT_1,
+        TCP_S_FIN_WAIT_2,
+        TCP_S_CLOSE_WAIT,
+        TCP_S_CLOSING,
+        TCP_S_LAST_ACK,
+        TCP_S_TIME_WAIT,
+        TCP_S_CLOSED,
 };
 
 struct socket_impl {
-    bool valid;
-    enum socket_state state;
+        list_node all_sockets;
 
-    int domain;
-    int type;
-    int protocol;
+        enum socket_state state;
 
-    mutex_t block_mtx; // cond_var?
+        int domain;
+        int type;
+        int protocol;
 
-    // IP {{
-    unsigned int ip_id;
-    be32 local_ip;
-    be16 local_port;
-    be32 remote_ip;
-    be16 remote_port;
-    // }}
+        mutex_t block_mtx; // cond_var?
 
-    // SOCK_DGRAM {{
-    list dgram_queue;  // datagram socket pks
-    // }}
+        // IP {{
+        unsigned int ip_id;
+        be32 local_ip;
+        be16 local_port;
+        be32 remote_ip;
+        be16 remote_port;
+        // }}
 
-    // TCP {{
-    list accept_queue; // accept() TCP_SYN pks
+        // SOCK_DGRAM {{
+        list dgram_queue;  // datagram socket pks
+        // }}
 
-    uint32_t send_seq; // SND.NXT - seq of next byte to send
-    uint32_t send_ack; // SND.UNA - seq of last byte sent acknowleged
-    uint32_t recv_seq; // RCV.NXT - seq of next byte to be recieved
-    uint16_t window_size;
+        // TCP {{
+        list accept_queue; // accept() TCP_SYN pks
 
-    enum tcp_state tcp_state;
+        uint32_t send_seq; // SND.NXT - seq of next byte to send
+        uint32_t send_ack; // SND.UNA - seq of last byte sent acknowleged
+        uint32_t recv_seq; // RCV.NXT - seq of next byte to be recieved
+        uint16_t window_size;
+
+        enum tcp_state tcp_state;
 
 #define TCP_RECV_BUF_LEN (64 * 1024)
-    uint32_t recv_buf_seq;
-    size_t recv_buf_len;
-    char *recv_buf;
+        uint32_t recv_buf_seq;
+        size_t recv_buf_len;
+        char *recv_buf;
 
-    bool tcp_psh;
+        bool tcp_psh;
 
-    // packets with seq > next expected seq.
-    list ooo_queue;
-    // packets that could be retransmitted if needed
-    list unacked_pks;
-    // }}
+        // packets with seq > next expected seq.
+        list ooo_queue;
+        // packets that could be retransmitted if needed
+        list unacked_pks;
+        // }}
 };
 
 // int x_socket(int domain, int type, int protocol);
@@ -84,20 +86,20 @@ int x_accept(struct socket_impl *, struct sockaddr *addr, socklen_t *addrlen);
 int x_connect(struct socket_impl *, const struct sockaddr *addr, socklen_t addrlen);
 ssize_t x_send(struct socket_impl *, const void *buf, size_t len, int flags);
 ssize_t x_sendto(struct socket_impl *, const void *buf, size_t len, int flags,
-        const struct sockaddr *dest_addr, socklen_t addrlen);
+                const struct sockaddr *dest_addr, socklen_t addrlen);
 ssize_t x_recv(struct socket_impl *, void *bud, size_t len, int flags);
 ssize_t x_recvfrom(struct socket_impl *, void *buf, size_t len, int flags,
-        struct sockaddr *src_addr, socklen_t *addrlen);
+                struct sockaddr *src_addr, socklen_t *addrlen);
 
 sysret sys_socket(int domain, int type, int protocol);
 sysret sys_bind(int fd, struct sockaddr *, size_t);
 sysret sys_connect(int fd, struct sockaddr *, size_t);
 sysret sys_send(int fd, const void *buf, size_t len, int flags);
 sysret sys_sendto(int fd, const void *buf, size_t len, int flags,
-        const struct sockaddr *, size_t);
+                const struct sockaddr *, size_t);
 sysret sys_recv(int fd, void *buf, size_t len, int flags);
 sysret sys_recvfrom(int fd, void *buf, size_t len, int flags,
-        struct sockaddr *, size_t *);
+                struct sockaddr *, size_t *);
 sysret sys_listen(int fd, int backlog);
 sysret sys_accept(int fd, struct sockaddr *addr, socklen_t *addrlen);
 
--- net/socket.c
+++ net/socket.c
@@ -1,1 +1,20 @@
 
+#include <basic.h>
+#include <net/core.h>
+#include <net/if.h>
+#include <net/socket.h>
+#include <ng/fs.h>
+#include <stdio.h>
+
+LIST_DEFINE(all_sockets);
+
+sysret sys_socket(int domain, int type, int protocol) {
+        struct file *node;
+        struct socket_impl *socket;
+        struct open_file *ofd;
+        int fd;
+        
+        return -ETODO;
+
+        // return fd;
+}
