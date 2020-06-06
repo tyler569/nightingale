51ac694bc4746b26afbe2dfcb26efb88f1fd473e
Tyler Philbrick <t5j6p9@gmail.com>
2020-06-06 10:22:01 -0700

    list: replace with better interface (less typeof!)

--- libc/include/list.h
+++ libc/include/list.h
@@ -1,225 +1,73 @@
-
 #pragma once
 #ifndef _LIST_H_
 #define _LIST_H_
 
-#include <basic.h>
-#include <assert.h>
-// #include <stdio.h>
-
 struct list {
         struct list *next;
-        struct list *prev;
+        struct list *previous;
 };
 
 typedef struct list list;
-typedef struct list list_n;    // REMOVE
-typedef struct list list_node; // REMOVE
+typedef struct list list_head;
+typedef struct list list_n;
+typedef struct list list_node;
 
 #define LIST_INIT(name) { &(name), &(name) }
 #define LIST_DEFINE(name) list name = LIST_INIT(name)
 
+#define container_of(type, node, ptr) \
+        (type *)((char *)(ptr) - offsetof(type, node))
 
-#define container_of(type, member, pointer) \
-        (type *)(((char *)pointer) - offsetof(type, member))
+#define list_head(type, node, ptr) \
+        container_of(type, node, (ptr)->next)
+#define list_next list_head
 
-#define list_entry(type, member, pointer) \
-        container_of(type, member, pointer)
+#define list_for_each(type, var, list, node) \
+        for ( \
+                type *var   = list_head(type, node, (list)), \
+                *__tmp = list_next(type, node, &var->node); \
+                &var->node != list; \
+                var = __tmp, \
+                __tmp = list_next(type, node, &__tmp->node) \
+        )
 
 static inline
-void _list_init(list *l) {
-        l->next = l;
-        l->prev = l;
+void list_insert(struct list *before, struct list *after, struct list *new_node) {
+        before->next = new_node;
+        new_node->previous = before;
+
+        new_node->next = after;
+        after->previous = new_node;
 }
 
 static inline
-void list_init(list *l) {
-        l->next = l;
-        l->prev = l;
+void list_append(struct list *head, struct list *new_node) {
+        list_insert(head->previous, head, new_node);
 }
 
 static inline
-list *list_head(list *l) {
-        return l->next;
+void list_prepend(struct list *head, struct list *new_node) {
+        list_insert(head, head->next, new_node);
 }
 
 static inline
-list *list_tail(list *l) {
-        return l->prev;
+void list_init(struct list *head) {
+        head->next = head;
+        head->previous = head;
 }
 
 static inline
-list *list_next(list *l) {
-        return l->next;
+void list_remove_between(struct list *previous, struct list *next) {
+        next->previous = previous;
+        previous->next = next;
 }
 
 static inline
-bool list_empty(list *l) {
-        return l == list_next(l);
+void list_remove(struct list *node) {
+        list_remove_between(node->previous, node->next);
+
+        node->next = 0;
+        node->previous = 0;
 }
 
-#define list_foreach_node(list, node) \
-        for (node = list_head(list); \
-             node != (list); \
-             node = list_next(node))
-
-
-static inline
-void list_remove(list *l) {
-        if (!l->next) {
-                assert(!l->prev);
-                return;
-        }
-        l->next->prev = l->prev;
-        l->prev->next = l->next;
-
-        l->next = l;
-        l->prev = l;
-}
-
-
-#define list_head_entry(type, list, member) \
-        list_entry(type, member, list_head(list))
-
-#define list_next_entry(item, member) \
-        list_entry(typeof(*(item)), member, (item)->member.next)
-
-
-#define list_head_entry_or_null(type, list, member) ({ \
-                list *__l = list; \
-                list_empty(__l) ? NULL : list_entry(type, __l, member); \
-        })
-
-
-#define list_foreach(list, var, member) \
-        for (var = list_head_entry(typeof(*var), (list), member); \
-             &var->member != (list); \
-             var = list_next_entry(var, member))
-
-#define list_foreach_safe(list, var, tmp, member) \
-        for (var = list_head_entry(typeof(*var), (list), member), \
-             tmp = list_next_entry(var, member); \
-             &var->member != (list); \
-             var = tmp, \
-             tmp = list_next_entry(tmp, member))
-
-static inline
-void _list_append(list *l, list_node *ln) {
-        ln->next = l;
-        ln->prev = l->prev;
-
-        l->prev->next = ln;
-        l->prev = ln;
-}
-
-#define list_append(list, pointer, member) \
-        _list_append(list, &(pointer)->member)
-
-static inline
-void _list_prepend(list *l, list_node *ln) {
-        ln->prev = l;
-        ln->next = l->next;
-
-        l->next->prev = ln;
-        l->next = ln;
-}
-
-#define list_prepend(list, pointer, member) \
-        _list_prepend(list, &(pointer)->member)
-
-
-static inline
-void list_delete(list *before, list *after) {
-        before->next = after;
-        after->prev = before;
-}
-
-static inline
-list *list_drop_head(list *l) {
-        if (list_empty(l)) return NULL;
-        assert(l->next && l->prev);
-
-        list *old = list_head(l);
-
-        // printf("dropping %p\n", old);
-        // printf("  l is %p\n", l);
-        // printf("  l->next->next is %p\n", l->next->next);
-
-        list_delete(l, l->next->next);
-        list_init(old);
-
-        return old;
-}
-
-#define list_pop_front(type, list, member) \
-        list_entry(type, member, list_drop_head(list))
-
-static inline
-list *list_drop_tail(list *l) {
-        if (list_empty(l)) return NULL;
-        assert(l->next && l->prev);
-
-        list *old = l->prev;
-
-        l->prev = l->prev->prev;
-        l->prev->next = l;
-
-        return old;
-}
-
-#define list_pop_back(type, list, member) \
-        list_entry(type, member, list_drop_tail(list))
-
-
-static inline
-int list_length(list *l) {
-        int count = 0;
-        list *node;
-
-        list_foreach_node(l, node) {
-                count += 1;
-        }
-        return count;
-}
-
-static inline
-void list_insert_after(list_node *ln, list_node *nln) {
-        nln->prev = ln;
-        nln->next = ln->next;
-
-        ln->next->prev = nln;
-        ln->next = nln;
-}
-
-static inline
-void list_insert_before(list_node *ln, list_node *nln) {
-        nln->next = ln;
-        nln->prev = ln->prev;
-
-        ln->prev->next = nln;
-        ln->prev = nln;
-}
-
-static inline
-void list_concat(list *dst, list *src) {
-        if (list_empty(src)) {
-                return;
-        }
-
-        list_node *shead = list_head(src);
-        list_node *stail = list_tail(src);
-
-        list_node *dhead = list_head(dst);
-        list_node *dtail = list_tail(dst);
-
-        dtail->next = shead;
-        shead->prev = dtail;
-
-        stail->next = dst;
-        dst->prev = stail;
-
-        _list_init(src);
-}
-
-#endif // _LIST_H_
-
+#endif
