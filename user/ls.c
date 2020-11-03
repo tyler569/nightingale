b0d9d92cc60d17f987782c3fefdf5a1ee1ded35f
Tyler Philbrick <t5j6p9@gmail.com>
2020-11-03 09:38:40 -0800

    ls: cleanup refactors

--- user/ls.c
+++ user/ls.c
@@ -4,6 +4,13 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+void check_err(int code, const char *message) {
+        if (code < 0) {
+                perror(message);
+                exit(EXIT_FAILURE);
+        }
+}
+
 const char filetype_sigils[] = {
         [FT_DIRECTORY]  = '/',
         [FT_BUFFER]     = ' ',
@@ -14,35 +21,41 @@ const char filetype_sigils[] = {
         [FT_PROC]       = '%',
 };
 
+char ft_sigil(struct ng_dirent *dirent) {
+        int type = dirent->type;
+        int perm = dirent->permissions;
+        if (type == FT_BUFFER && (perm & USR_EXEC)) {
+                return '*';
+        } else {
+                return filetype_sigils[type];
+        }
+}
+
 int main(int argc, char **argv) {
+        int ttyout = isatty(STDOUT_FILENO);
         int fd;
+
         if (argc == 1) {
                 fd = open(".", O_RDONLY);
         } else {
                 fd = open(argv[1], O_RDONLY);
         }
+        check_err(fd, "open");
 
-        if (fd < 0) {
-                perror("open()");
-                return EXIT_FAILURE;
-        }
-
-        struct ng_dirent dirent_buf[64];
-
-        int entries = getdirents(fd, dirent_buf, 64);
+        struct ng_dirent dirent_buf[128];
+        int entries = readdir(fd, dirent_buf, 128);
+        check_err(entries, "readdir");
 
         for (int i=0; i<entries; i++) {
-                printf("%s", dirent_buf[i].filename);
-                if (isatty(STDOUT_FILENO)) {
-                        int type = dirent_buf[i].type;
-                        int perm = dirent_buf[i].permissions;
-                        if (type == FT_BUFFER && (perm & USR_EXEC)) {
-                                printf("*");
-                        } else {
-                                printf("%c", filetype_sigils[type]);
-                        }
+                struct ng_dirent *entry = &dirent_buf[i];
+                if (entry->filename[0] == '.') {
+                        // continue unless -a
                 }
-                printf("\n");
+                if (ttyout) {
+                        printf("%s%c\n", entry->filename, ft_sigil(entry));
+                } else {
+                        printf("%s\n", entry->filename);
+                }
         }
 
         return EXIT_SUCCESS;
