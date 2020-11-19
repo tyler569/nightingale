843ce67ad054a0a91e4434dd55c24889f14bb26c
Tyler Philbrick <t5j6p9@gmail.com>
2020-11-18 21:40:01 -0800

    signal: don't declare userland functions in the kernel

--- libc/include/signal.h
+++ libc/include/signal.h
@@ -49,11 +49,6 @@ typedef uint32_t sigset_t;
 typedef void (*sighandler_t)(int);
 typedef atomic_int sig_atomic_t;
 
-sighandler_t sigaction(int signum, sighandler_t handler, int flags);
-sighandler_t signal(int signum, sighandler_t handler);
-int kill(pid_t pid, int sig);
-int raise(int signal);
-
 // sigset operations
 int sigemptyset(sigset_t *set);
 int sigfillset(sigset_t *set);
@@ -61,6 +56,15 @@ int sigaddset(sigset_t *set, int signum);
 int sigdelset(sigset_t *set, int signum);
 int sigismember(const sigset_t *set, int signum);
 
+#ifndef __kernel__
+
 int sigprocmask(int op, const sigset_t *new, sigset_t *old);
+
+sighandler_t sigaction(int signum, sighandler_t handler, int flags);
+sighandler_t signal(int signum, sighandler_t handler);
+int kill(pid_t pid, int sig);
+int raise(int signal);
+
+#endif // __kernel__
 
 #endif // _SIGNAL_H_
