2b1f4050efc6168b91a066253d17cd6be7a0375e
Tyler Philbrick <t5j6p9@gmail.com>
2020-11-22 08:19:17 -0800

    signal: spacing fixes

--- kernel/include/ng/signal.h
+++ kernel/include/ng/signal.h
@@ -20,15 +20,11 @@ struct signal_context {
 };
 
 int signal_send(pid_t pid, int signal);
-
 int signal_send_pgid(pid_t pgid, int signal);
-
 int signal_send_th(struct thread *th, int signal);
-
 void signal_self(int signal);
 
 int handle_pending_signals(void);
-
 void handle_signal(int signal, sighandler_t);
 
 void do_signal_call(int signal, sighandler_t);
