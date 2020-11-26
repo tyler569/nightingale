8defa28c447f1eec190edd2048ba86fd9c2f7501
Tyler Philbrick <t5j6p9@gmail.com>
2020-11-25 21:04:53 -0800

    ls: use the new getopt and redirect_output to make ls nice

--- user/ls.c
+++ user/ls.c
@@ -1,5 +1,7 @@
 #include <dirent.h>
 #include <fcntl.h>
+#include <nightingale.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -10,10 +12,17 @@ void check_err(int code, const char *message) {
     }
 }
 
+// clang-format off
 const char filetype_sigils[] = {
-    [FT_DIRECTORY] = '/', [FT_BUFFER] = ' ', [FT_CHARDEV] = '^', [FT_TTY] = '#',
-    [FT_SOCKET] = ':',    [FT_PIPE] = '&',   [FT_PROC] = '%',
+    [FT_DIRECTORY] = '/',
+    [FT_BUFFER]    = ' ',
+    [FT_CHARDEV]   = '^',
+    [FT_TTY]       = '#',
+    [FT_SOCKET]    = ':',
+    [FT_PIPE]      = '&',
+    [FT_PROC]      = '%',
 };
+// clang-format on
 
 char ft_sigil(struct ng_dirent *dirent) {
     int type = dirent->type;
@@ -25,14 +34,46 @@ char ft_sigil(struct ng_dirent *dirent) {
     }
 }
 
-int main(int argc, char **argv) {
-    int ttyout = isatty(STDOUT_FILENO);
-    int fd;
+void help(const char *progname) {
+    fprintf(stderr, "%s: usage ls [-alF]\n"
+                    "  -a     Show all files\n"
+                    "  -l     List files long form\n"
+                    "  -F     Show filetype sigils\n", progname);
+}
 
-    if (argc == 1) {
+int main(int argc, char **argv) {
+    bool all = false;
+    // Intentionally checking this before potentially redirecting output
+    bool classify = isatty(STDOUT_FILENO);
+    bool long_ = !classify;
+    int fd, opt;
+
+    while ((opt = getopt(argc, argv, "alF")) != -1) {
+        switch (opt) {
+        case 'a':
+            all = true;
+            break;
+        case 'l':
+            long_ = true;
+            break;
+        case 'F':
+            classify = true;
+            break;
+        default:
+            help(argv[0]);
+            return 0;
+        }
+    }
+
+    if (!long_) {
+        redirect_output_to((char*[]){"/bin/column", NULL});
+    }
+
+    if (!argv[optind]) {
         fd = open(".", O_RDONLY);
     } else {
-        fd = open(argv[1], O_RDONLY);
+        // TODO: loop over remaining arguments and list them all
+        fd = open(argv[optind], O_RDONLY);
     }
     check_err(fd, "open");
 
@@ -43,9 +84,9 @@ int main(int argc, char **argv) {
     for (int i = 0; i < entries; i++) {
         struct ng_dirent *entry = &dirent_buf[i];
         if (entry->filename[0] == '.') {
-            // continue unless -a
+            if (!all) continue;
         }
-        if (ttyout) {
+        if (classify) {
             printf("%s%c\n", entry->filename, ft_sigil(entry));
         } else {
             printf("%s\n", entry->filename);
