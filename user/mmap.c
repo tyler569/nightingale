9a94608935032ac8ffac97c497c8ba68b055e47b
Tyler Philbrick <t5j6p9@gmail.com>
2020-11-29 18:56:31 -0800

    user/mmap: test program for file mmap

---
+++ user/mmap.c
@@ -0,0 +1,20 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+int main() {
+    int fd = open("text_file", O_RDONLY);
+    if (fd < 0) {
+        perror("open");
+        return 1;
+    }
+    void *addr = mmap(NULL, 4096, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (addr == MAP_FAILED) {
+        perror("mmap");
+        return 1;
+    }
+
+    printf("%s", (char *)addr);
+}
