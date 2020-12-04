a9a9f21a5b4e9d8d4c190fe214eb133aadaf32d8
Tyler Philbrick <t5j6p9@gmail.com>
2020-12-03 17:25:54 -0800

    threads: add nice convenient options

--- user/threads.c
+++ user/threads.c
@@ -5,25 +5,60 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+bool go_slow = true;
+
+void slow() {
+    for (int i = 0; i < 1000000; i++) asm("");
+}
+
 void print_my_letter(char c) {
     for (int j = 0; j < 10; j++) {
-        for (int i = 0; i < 500000; i++)
-            ;
+        if (go_slow) slow();
 
         printf("%c", c);
     }
     exit(0);
 }
 
+void help(const char *progname) {
+    fprintf(stderr,
+            "%s: usage threads [-hfw] [-n THREADS]\n"
+            "  -h            Show this help\n"
+            "  -f            Don't slow prints\n"
+            "  -n THREADS    Start THREADS processes\n"
+            "  -w            Wait between each fork\n",
+            progname);
+}
+
 int main(int argc, char **argv) {
     int pid = getpid();
-    int wait_each = argc > 1;
     int child;
+    int nthreads = 26;
+    bool wait_each = false;
+    char c, l = 'A';
+
+    while ((c = getopt(argc, argv, "hfn:w")) != -1) {
+        switch (c) {
+        case 'f':
+            go_slow = false; break;
+        case 'n':
+            nthreads = strtol(optarg, NULL, 10); break;
+        case 'w':
+            wait_each = true; break;
+        case 'h': // FALLTHROUGH
+        case '?':
+            help(argv[0]);
+            return 0;
+        }
+    }
+
     setpgid(pid, pid);
 
-    for (char c = 'A'; c <= 'Z'; c++) {
-        if (!(child = fork())) { print_my_letter(c); }
-        if (wait_each) { waitpid(child, &child, 0); }
+    for (int i=0; i<nthreads; i++) {
+        if (!(child = fork())) print_my_letter(l);
+        if (wait_each) waitpid(child, &child, 0);
+        l += 1;
+        if (l == 'Z') l = 'A';
     }
 
     while (errno != ECHILD) {
