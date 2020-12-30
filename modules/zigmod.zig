6597dbc85a0271cf140bf5c5ab398535e9582550
Tyler Philbrick <t5j6p9@gmail.com>
2020-12-30 14:34:42 -0800

    zigmod: update for api changes

--- modules/zigmod.zig
+++ modules/zigmod.zig
@@ -1,16 +1,16 @@
 // Not building by default since a zig environment is not common or something
 // I want to require.
 //
-// zig build-obj modules/zigmod.zig -target x86_64-freestanding -Isysroot/usr/include --output-dir sysroot/usr/bin/
+// zig build-obj modules/zigmod.zig -target x86_64-freestanding -Isysroot/usr/include --output-dir sysroot/bin/
 const ng = @cImport({
     @cInclude("basic.h");
     @cInclude("stdio.h");
     @cInclude("ng/mod.h");
 });
 
-export fn init(mod: [*c]ng.mod) ng.modinit_status {
+export fn init(mod: [*c]ng.mod) c_int {
     _ = ng.printf("Hello World from a zig kernel module\n");
-    return @intToEnum(ng.modinit_status, ng.MODINIT_SUCCESS);
+    return ng.MODINIT_SUCCESS;
 }
 
 export const modinfo: ng.modinfo = ng.modinfo{
