9266ecfdaff6481099df7b5667ceada43e51d1f5
Tyler Philbrick <t5j6p9@gmail.com>
2020-12-30 19:43:34 -0800

    procmod: add procfile module back

--- modules/procmod.c
+++ modules/procmod.c
@@ -1,24 +1,20 @@
-
 #include <basic.h>
 #include <ng/mod.h>
 #include <ng/fs.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void module_procfile(struct open_file *ofd) {
-        ofd->buffer = malloc(1024);
-        ofd->length = sprintf(
-                        ofd->buffer,
-                        "Hello World from a kernel module\n");
+void module_procfile(struct open_file *ofd, void *_) {
+    proc_sprintf(ofd, "Hello World from a kernel module\n");
 }
 
-enum modinit_status modinit(struct mod *_) {
-        make_procfile("mod", module_procfile, NULL);
-        return MODINIT_SUCCESS;
+int modinit(struct mod *_) {
+    make_procfile("mod", module_procfile, NULL);
+    return MODINIT_SUCCESS;
 }
 
 struct modinfo modinfo = {
-        .name = "procfile module",
-        .modinit = modinit,
+    .name = "procmod",
+    .init = modinit,
 };
 
