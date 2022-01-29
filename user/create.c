bad4bfdacd6ce0db0f7e0bb959ad655dddeb8916
Tyler Philbrick <t5j6p9@gmail.com>
2022-01-29 04:39:31 +0000

    create: wait on the created child process

--- user/create.c
+++ user/create.c
@@ -1,6 +1,7 @@
 #include <nightingale.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 int main(int argc, char **argv) {
@@ -12,6 +13,7 @@ int main(int argc, char **argv) {
     pid_t pid = create(argv[1]);
     procstate(pid, PS_COPYFDS);
     procstate(pid, PS_SETRUN);
+    waitpid(pid, NULL, 0);
 
     return 0;
 }
