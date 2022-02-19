febc8b8305c9c769a9ed73d2f001637f9eff37fa
Tyler Philbrick <t5j6p9@gmail.com>
2022-02-19 12:51:16 -0800

    random: further improve random init and generation
    Instead of a global `counter` (which will cause duplicates after each
    thread increments the counter), let's use a global `nonce`)

--- kernel/random.c
+++ kernel/random.c
@@ -11,12 +11,15 @@
 char random_pool[256];
 
 void add_to_random(const char *buffer, size_t len) {
+    for (size_t i = 0; i < len; i++) {
+        random_pool[i & POOL_MASK] += buffer[i];
+    }
+
     struct chacha20_state state = {{
         0x61707865, 0x3320646e, 0x79622d32, 0x6b206574,
     }};
 
-    memcpy(state.n + 4, buffer, min(len, 32));
-    state.n[13] = 0x55;
+    memcpy(state.n + 4, buffer, min(len, 48));
 
     char buf[64];
     for (int i = 0; i < 4; i++) {
@@ -28,26 +31,22 @@ void add_to_random(const char *buffer, size_t len) {
 }
 
 void random_dance() {
-    for (int i = 0; i < 1000; i++) {
-        for (int loop = 0; loop < 100 * i; loop++) asm volatile ("");
+    for (int i = 0; i < 100; i++) {
         uint64_t time = rdtsc();
         add_to_random((char *)&time, 8);
     }
 }
 
-atomic_int counter = 1;
+atomic_long global_nonce = 1;
 
 size_t get_random(char *buffer, size_t len) {
-    atomic_fetch_add(&counter, 1);
+    long nonce[2] = {0, 1};
+    nonce[0] = atomic_fetch_add(&global_nonce, 1);
     struct chacha20_state state = init(
             random_pool,
-            random_pool + 32,
-            atomic_load(&counter)
+            (char *)nonce,
+            1
     );
-    
-    char internal[32];
-    chacha20_keystream(&state, internal, 32);
-    add_to_random(internal, 32);
 
     chacha20_keystream(&state, buffer, len);
     return len;
