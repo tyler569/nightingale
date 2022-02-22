9cad7b6ee5c5a5fb1931be84cee86ed4bd8f5280
Tyler Philbrick <t5j6p9@gmail.com>
2022-02-21 22:57:00 -0800

    user/head: rewrite head and support character limits
    It's not perfect, but it works for trivial cases.
    
    If you `head` an infinite character device it both gives the wrong data
    and it hangs forever, but I suspect that has more to do with pipes than
    this, this program runs on Linux, so it must be fine tm.

--- user/head.c
+++ user/head.c
@@ -1,26 +1,67 @@
-// vim: sw=4:ts=4:sts=4 :
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int main() {
-    char buffer[1024];
-    char output[1024];
-    size_t len;
+#define BUF 1024
 
+char buffer[BUF];
+
+void copy_n_lines(FILE *out, FILE *in, int lines) {
+    int newlines_seen = 0;
+    while (newlines_seen < lines) {
+        char *c = fgets(buffer, BUF, in);
+        if (!c) {
+            perror("fgets");
+            exit(1);
+        }
+        int len = strlen(buffer);
+        int n = fwrite(buffer, 1, len, out);
+        if (n < 0) {
+            perror("fwrite");
+            exit(1);
+        }
+        if (memchr(buffer, '\n', len)) {
+            newlines_seen++;
+        }
+    }
+}
+
+void copy_n_characters(FILE *out, FILE *in, int chars) {
+    int cursor = 0;
+    while (cursor < chars) {
+        int len = fread(buffer, 1, BUF, in);
+        int n_write = (chars - cursor) > len ? len : (chars - cursor);
+        fwrite(buffer, 1, n_write, out);
+        cursor += n_write;
+    }
+}
+
+int main(int argc, char **argv) {
+    int n_c = -1;
     int n_nl = 10;
 
-    while (n_nl > 0 && !feof(stdin)) {
-        len = fread(buffer, 1, 1024, stdin);
-        int i;
-        for (i = 0; i < len; i++) {
-            if (buffer[i] == '\n')
-                n_nl -= 1;
-            output[i] = buffer[i];
-            if (n_nl == 0) {
-                i += 1;
-                break;
-            }
+    int c;
+    while ((c = getopt(argc, argv, "n:c:")) != -1) {
+        switch (c) {
+        case 'n':
+            n_nl = atoi(optarg);
+            break;
+        case 'c':
+            n_c = atoi(optarg);
+            n_nl = -1;
+            break;
+        default:
+            fprintf(stderr, "oops\n");
         }
-        fwrite(output, 1, i, stdout);
     }
+
+    if (n_c > 0) {
+        copy_n_characters(stdout, stdin, n_c);
+    } else {
+        copy_n_lines(stdout, stdin, n_nl);
+    }
+
     return 0;
 }
