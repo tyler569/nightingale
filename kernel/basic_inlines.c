683e2a5eded3a363665792345574145314468c4f
Tyler Philbrick <t5j6p9@gmail.com>
2022-02-28 13:02:25 -0800

    inline functions: provide instantiation of inline functions in basic
    You're supposed to always instantiate inline functions in header files
    in one TU so they exist as external symbols and can be linked without
    inlining if necessary; it hasn't yet been necessary, but let's do it as
    we're supposed to.

---
+++ kernel/basic_inlines.c
@@ -0,0 +1,10 @@
+#include <basic.h>
+
+extern inline bool IS_ERROR(void *R);
+extern inline intptr_t max(intptr_t a, intptr_t b);
+extern inline intptr_t min(intptr_t a, intptr_t b);
+extern inline uintptr_t umax(uintptr_t a, uintptr_t b);
+extern inline uintptr_t umin(uintptr_t a, uintptr_t b);
+extern inline uintptr_t round_down(uintptr_t val, uintptr_t place);
+extern inline uintptr_t round_up(uintptr_t val, uintptr_t place);
+extern inline void delay(int usec);
