7b6d489450f791265a037cf54e50753971dc96af
Tyler Philbrick <t5j6p9@gmail.com>
2022-03-20 16:04:25 -0700

    fs: remove inodes from their fs list before deleting them

--- kernel/fs/inode.c
+++ kernel/fs/inode.c
@@ -82,6 +82,7 @@ void maybe_delete_inode(struct inode *inode)
 
     if (inode->symlink_destination)
         free((void *)inode->symlink_destination);
+    list_remove(&inode->fs_inodes);
     //#include <stdio.h>
     // printf("inode (%c) freed\n", __filetype_sigils[inode->type]);
     free(inode);
