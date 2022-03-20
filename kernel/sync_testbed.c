c12e7f42c6b6e5ff6aca49c8170aa4d4f922ee72
Tyler Philbrick <t5j6p9@gmail.com>
2022-03-20 16:04:49 -0700

    tests: sync testbed is doing nothing since co-op mtasking

--- kernel/sync_testbed.c
+++ kernel/sync_testbed.c
@@ -7,66 +7,54 @@ void sync_test_controller(void *);
 
 void run_sync_tests(void) { kthread_create(sync_test_controller, NULL); }
 
-mutex_t join_a_mutex;
-mutex_t join_b_mutex;
+mutex_t join_mutex;
 mutex_t new_m;
-condvar_t join_a;
-condvar_t join_b;
+condvar_t join_cv;
+atomic_int n_threads;
 
-atomic_int unsynchronized = 0;
+int unsynchronized = 0;
 atomic_int synchronized = 0;
 
-const long loops = 100000;
+const long loops = 10000000;
 const long print = loops / 10;
 
-void sync_thread_a(void *);
-void sync_thread_b(void *);
+void sync_thread(void *);
 
 void sync_test_controller(void *_)
 {
     mutex_init(&new_m);
-    mutex_init(&join_a_mutex);
-    mutex_init(&join_b_mutex);
-    cv_init(&join_a);
-    cv_init(&join_b);
+    mutex_init(&join_mutex);
+    cv_init(&join_cv);
 
-    kthread_create(sync_thread_a, NULL);
-    kthread_create(sync_thread_b, NULL);
+    int n = 3;
+    atomic_store(&n_threads, n);
+    for (int i = 0; i < n; i++)
+        kthread_create(sync_thread, NULL);
 
-    mutex_lock(&join_a_mutex);
-    cv_wait(&join_a, &join_a_mutex);
-    mutex_unlock(&join_a_mutex);
+    while (atomic_load(&n_threads) > 0) {
+        mutex_lock(&join_mutex);
+        cv_wait(&join_cv, &join_mutex);
+    }
 
-    mutex_lock(&join_b_mutex);
-    cv_wait(&join_b, &join_b_mutex);
-    mutex_unlock(&join_b_mutex);
-
-    assert(synchronized == loops * 2);
+    assert(synchronized == loops * n);
     // assert(unsynchronized == loops * 2);
 
-    // printf("unsync: %i\n", unsynchronized);
-    // printf("  sync: %i\n", synchronized);
+    printf("unsync: %i\n", unsynchronized);
+    printf("  sync: %i\n", synchronized);
     kthread_exit();
 }
 
-void sync_thread_a(void *_)
+void sync_thread(void *_)
 {
+    for (int i = 0; i < loops; i++)
+        unsynchronized++;
+
     for (int i = 0; i < loops; i++) {
         mutex_lock(&new_m);
         synchronized++;
         mutex_unlock(&new_m);
     }
-    cv_signal(&join_a);
-    kthread_exit();
-}
-
-void sync_thread_b(void *_)
-{
-    for (int i = 0; i < loops; i++) {
-        mutex_lock(&new_m);
-        synchronized++;
-        mutex_unlock(&new_m);
-    }
-    cv_signal(&join_b);
+    atomic_fetch_sub(&n_threads, 1);
+    cv_signal(&join_cv);
     kthread_exit();
 }
--- kernel/tests.c
+++ kernel/tests.c
@@ -12,31 +12,11 @@
 
 void run_sync_tests(void);
 
-void test_kernel_thread(void *arg)
+noreturn void test_kernel_thread(void *arg)
 {
     const char *message = arg;
     assert(strcmp(arg, "get a cat") == 0);
     kthread_exit();
-}
-
-void lots_of_threads(void *message)
-{
-    printf("%s", (char *)message);
-    kthread_exit();
-}
-
-noreturn void test_sleepy_thread(void *_)
-{
-    while (true) {
-        printf("sleepy thread");
-        sleep_thread(seconds(1));
-    }
-}
-
-void print_key(interrupt_frame *frame, void *_x)
-{
-    char scancode = inb(0x80);
-    printf("keyboard interrupt: %c\n", scancode);
 }
 
 void run_spalloc_test()
@@ -72,48 +52,6 @@ void run_spalloc_test()
 
 void run_all_tests()
 {
-    run_sync_tests();
     run_spalloc_test();
     kthread_create(test_kernel_thread, "get a cat");
-    // kthread_create(test_sleepy_thread, NULL);
-
-    for (int i = 0; i < 50; i++) {
-        kthread_create(lots_of_threads, "");
-        kthread_create(lots_of_threads, "");
-    }
-
-    pic_irq_unmask(IRQ_KEYBOARD);
-    irq_install(IRQ_KEYBOARD, print_key, NULL);
-}
-
-void test_oldmutex_speed(int loops)
-{
-    mutex_t mtx = make_mutex();
-    uint64_t tsc = rdtsc();
-    for (int i = 0; i < loops; i++) {
-        mutex_lock(&mtx);
-        mutex_unlock(&mtx);
-    }
-    printf("%5ik loops: %-12zu ", loops / 1000, rdtsc() - tsc);
-}
-
-void test_newmutex_speed(int loops)
-{
-    newmutex_t mtx;
-    newmutex_init(&mtx);
-    uint64_t tsc = rdtsc();
-    for (int i = 0; i < loops; i++) {
-        newmutex_lock(&mtx);
-        newmutex_unlock(&mtx);
-    }
-    printf("%-12zu\n", rdtsc() - tsc);
-}
-
-void test_mutex_speeds()
-{
-    printf("              %12s %12s\n", "old", "new");
-    for (int i = 1000; i < 10000000; i *= 10) {
-        test_oldmutex_speed(i);
-        test_newmutex_speed(i);
-    }
 }
