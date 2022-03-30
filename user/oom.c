eff373cc8ac1a414d1932c1de00867b162cda4fe
Tyler Philbrick <t5j6p9@gmail.com>
2022-03-30 00:04:31 -0700

    user/oom: print allocated pointers
    Clang seems to be able to optimize out the allocations if you only ever
    write to them, so this program was doing nothing but printing out some
    big numbers. Printing the pointer values seems to convince it to emit
    real malloc's.

--- user/oom.c
+++ user/oom.c
@@ -4,8 +4,9 @@
 int main()
 {
     for (size_t i = 1; i != 0; i <<= 1) {
-        printf("allocating %zu\n", i);
+        printf("allocating %zu ", i);
         char *x = malloc(i);
+        printf("%p\n", x);
         for (size_t n = 0; n < i; n++) {
             x[n] = 1;
         }
