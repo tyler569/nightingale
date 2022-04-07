a70e6370a8a8a2c458d9ca3a60f184c9cdbb5085
Tyler Philbrick <t5j6p9@gmail.com>
2022-04-06 22:47:03 -0700

    user/float: clean up float program

--- user/float.c
+++ user/float.c
@@ -6,17 +6,12 @@ int main()
 {
     double x = sin(4.0);
 
-    // if (x > 0.0) {
-    //     printf("ERROR ERROR\n");
-    // }
-
-    // printf("%lf\n", sin(4.0));
-
     while (true) {
         double pos = 20 * (sin(x) + 1.5);
-        printf("%02i %lf %.*s*\n", (int)pos, pos, (int)(pos),
-            "                                                                  "
-            "         ");
+        printf("%02i %lf ", (int)pos, pos);
+        for (int i = 0; i < pos; i++)
+            printf(" ");
+        printf("*\n");
         x += 0.1;
     }
     return 0;
