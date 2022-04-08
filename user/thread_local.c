c51ba0a3b399a4cbbaacab8530195e5c468eb04c
Tyler Philbrick <t5j6p9@gmail.com>
2022-04-07 21:36:04 -0700

    thread_local: trial a real TCB allocation function

--- user/thread_local.c
+++ user/thread_local.c
@@ -1,52 +1,53 @@
+#include <basic.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <nightingale.h>
 #include <unistd.h>
 
 int __ng_settls(void *base);
-
+struct tcb;
+struct tcb *alloc_tcb(void);
+_Thread_local int tls;
+_Noreturn void exit_thread(int code);
 char stack[8192];
 
-int tls_storage[2] = { 0 };
-
-void *tls_a_ptr = &tls_storage[1];
-void *tls_b_ptr = &tls_storage[2];
-
-_Thread_local int tls;
-
-_Noreturn void exit_thread(int code);
-
-int use_tls(void *arg)
+int use_tls(void)
 {
-    int v = *(int *)arg;
-    if (v)
-        __ng_settls(&tls_b_ptr);
-
     int tid = gettid();
     printf("tid is %i, tls is ", tid);
     tls = 10 + tid;
     printf("%i\n", tls);
-
-    if (v)
-        exit_thread(0);
     return 0;
 }
 
-int true_ = 1;
-int false_ = 0;
+int thread(void *arg)
+{
+    alloc_tcb();
+    use_tls();
+    exit_thread(0);
+}
 
 int main()
 {
-    __ng_settls(&tls_a_ptr);
-    clone(use_tls, stack + 8192, 0, &true_);
-    use_tls(&false_);
+    alloc_tcb();
+    clone(thread, stack + 8192, 0, NULL);
+    use_tls();
 
     sleepms(30);
+}
 
-    printf("after: [");
-    for (int i = 0; i < 2; i++) {
-        printf("%i", tls_storage[i]);
-        if (i < 1)
-            printf(", ");
-    }
-    printf("]\n");
+struct tcb {
+    struct tcb *self;
+    void *base;
+};
+
+struct tcb *alloc_tcb(void)
+{
+    void *base = calloc(1, 1024);
+    struct tcb *tcb = PTR_ADD(
+        base, round_down(1024 - sizeof(struct tcb), _Alignof(struct tcb)));
+    tcb->self = tcb;
+    tcb->base = base;
+    __ng_settls(tcb);
+    return tcb;
 }
