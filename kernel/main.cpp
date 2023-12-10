9f664370f33058c385e7d11ffb10c4393d5530a1
Tyler Philbrick <git@choam.me>
2023-12-09 21:40:59 -0800

    video: factor video code out of main
    It can't live here forever, but it can't like in main anymore at all.

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,4 +1,3 @@
-#include "nx/memory.h"
 #include <elf.h>
 #include <ng/arch.h>
 #include <ng/commandline.h>
@@ -26,6 +25,7 @@
 #include <nx/atomic.h>
 #include <nx/functional.h>
 #include <nx/list.h>
+#include <nx/memory.h>
 #include <nx/print.h>
 #include <nx/string.h>
 #include <nx/vector.h>
@@ -100,37 +100,6 @@ extern "C" [[noreturn]] void kernel_main(void)
     longjump_kcode((uintptr_t)real_main, (uintptr_t)&hhstack_top);
 }
 
-#define TEXT_SCALE 1
-
-uint32_t *fb;
-uint32_t width, height;
-void video_print(uint32_t x, uint32_t y, const char *str)
-{
-    extern unsigned char font8x8_basic[128][8];
-
-    for (size_t i = 0; i < strlen(str); i++) {
-        for (size_t j = 0; j < 8 * TEXT_SCALE; j++) {
-            for (size_t k = 0; k < 8 * TEXT_SCALE; k++) {
-                if (font8x8_basic[(unsigned char)str[i]][j / TEXT_SCALE]
-                    & (1 << (k / TEXT_SCALE)))
-                    fb[(y + j) * width + i * 8 * TEXT_SCALE + x + k]
-                        = 0xffffffff;
-            }
-        }
-    }
-}
-
-void video_scroll(uint32_t lines)
-{
-    lines *= 8 * TEXT_SCALE;
-    for (size_t i = 0; i < height - lines; i++) {
-        memcpy(fb + i * width, fb + (i + lines) * width, width * 4);
-    }
-    for (size_t i = height - lines; i < height; i++) {
-        memset(fb + i * width, 0, width * 4);
-    }
-}
-
 [[noreturn]] void real_main()
 {
     printf("real_main\n");
@@ -164,40 +133,6 @@ void video_scroll(uint32_t lines)
         printf("threads: usermode thread installed\n");
         printf("initialization took: %li\n", rdtsc() - tsc);
         printf("cpu: allowing irqs\n");
-    }
-
-    {
-        uint32_t pitch, bpp;
-        void *address;
-        limine_framebuffer(&width, &height, &bpp, &pitch, &address);
-
-        printf("framebuffer: %ux%u, %u bpp, pitch %u, address %p\n", width,
-            height, bpp, pitch, address);
-
-        if (bpp != 32)
-            panic("framebuffer bpp is not 32");
-
-        fb = (uint32_t *)address;
-
-        video_print(0, 0, "Hello world!");
-        video_print(0, 16, "abcdefghijklmnopqrstuvwxyz");
-        video_print(0, 32, "0123456789");
-        video_print(0, 48, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-        video_print(0, 64, "`~!@#$%^&*(){}[]_+-=:;\"'<>,.?/\\");
-        video_print(0, 80, "video_print(128, 128, \"Hello world!\");");
-
-        int y = 96;
-
-        char buf[64];
-        for (int i = 0; i < 30; i++) {
-            sprintf(buf, "Hello world! %i", i);
-            video_print(0, y, buf);
-            y += 8 * TEXT_SCALE;
-            if (y >= height) {
-                y -= 8 * TEXT_SCALE;
-                video_scroll(1);
-            }
-        }
     }
 
     enable_irqs();
