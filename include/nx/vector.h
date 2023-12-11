a763fe3ff8335ef34be06b2ca41340d46a808661
Tyler Philbrick <git@choam.me>
2023-12-11 00:13:37 -0800

    nx::vector: add pop_front and pop_back

--- include/nx/vector.h
+++ include/nx/vector.h
@@ -10,21 +10,21 @@ namespace nx {
 
 template <class T> class vector {
 protected:
-    size_t m_len;
-    size_t m_cap;
+    size_t m_size;
+    size_t m_capacity;
     T *m_elements;
 
 public:
     constexpr vector()
-        : m_len(0)
-        , m_cap(0)
+        : m_size(0)
+        , m_capacity(0)
         , m_elements(nullptr)
     {
     }
 
     explicit vector(size_t len)
-        : m_len(len)
-        , m_cap(len)
+        : m_size(len)
+        , m_capacity(len)
         , m_elements(new T[len])
     {
     }
@@ -33,36 +33,60 @@ public:
     using const_iterator = const T *;
 
     iterator begin() const { return m_elements; }
-    iterator end() const { return m_elements + m_len; }
+    iterator end() const { return m_elements + m_size; }
     const_iterator cbegin() const { return m_elements; }
-    const_iterator cend() const { return m_elements + m_len; }
+    const_iterator cend() const { return m_elements + m_size; }
 
     void expand(size_t new_cap)
     {
         if (new_cap < 16)
             new_cap = 16;
-        if (m_cap >= new_cap)
+        if (m_capacity >= new_cap)
             return;
 
         m_elements = (T *)realloc(m_elements, new_cap * sizeof(T *));
-        m_cap = new_cap;
+        m_capacity = new_cap;
     }
 
     void push_back(const T &value)
     {
-        while (m_len >= m_cap)
-            expand(m_cap * 2);
+        while (m_size >= m_capacity)
+            expand(m_capacity * 2);
 
-        m_elements[m_len++] = value;
+        m_elements[m_size++] = value;
+    }
+
+    void pop_back()
+    {
+        if (m_size == 0)
+            return;
+
+        m_size--;
+    }
+
+    void pop_front()
+    {
+        if (m_size == 0)
+            return;
+
+        for (size_t i = 0; i < m_size - 1; i++) {
+            m_elements[i] = m_elements[i + 1];
+        }
+
+        m_size--;
     }
 
     T &operator[](size_t index) const { return m_elements[index]; }
-    [[nodiscard]] size_t size() const { return m_len; }
-    [[nodiscard]] size_t capacity() const { return m_cap; }
+    [[nodiscard]] size_t size() const { return m_size; }
+    [[nodiscard]] size_t capacity() const { return m_capacity; }
     [[nodiscard]] T *data() { return m_elements; }
-    void clear() { m_len = 0; }
+    void clear() { m_size = 0; }
+    [[nodiscard]] bool empty() const { return m_size == 0; }
 
-    void debug_print() { print("vector: %/% d=%\n", m_len, m_cap, m_elements); }
+    void debug_print()
+    {
+        print("vector: %/% d=%\n", m_size, m_capacity, m_elements);
+    }
 };
 
 }
