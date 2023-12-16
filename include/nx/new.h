403956549d2d5bd7ca0435db0251103aedeb0417
Tyler Philbrick <git@choam.me>
2023-12-16 18:53:02 +0000

    nx/new: mark operators nodiscard and noexcept where possible

--- include/nx/new.h
+++ include/nx/new.h
@@ -4,11 +4,18 @@
 
 #include <stddef.h>
 
-void *operator new(size_t size);
-void *operator new[](size_t size);
+[[nodiscard]] void *operator new(size_t size);
+[[nodiscard]] void *operator new[](size_t size);
 void operator delete(void *ptr) noexcept;
 void operator delete[](void *ptr) noexcept;
-inline void *operator new(size_t count, void *ptr) { return ptr; }
-inline void *operator new[](size_t count, void *ptr) { return ptr; }
+
+[[nodiscard]] inline void *operator new(size_t count, void *ptr) noexcept
+{
+    return ptr;
+}
+[[nodiscard]] inline void *operator new[](size_t count, void *ptr) noexcept
+{
+    return ptr;
+}
 
 #endif // NX_NEW_H
