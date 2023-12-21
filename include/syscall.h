97abc7243f5a2ee8de691d0a3b81060cba97fc84
Tyler Philbrick <git@choam.me>
2023-12-21 14:03:50 +0000

    syscall: clean up & deduplicate
    See, all those registers are used for scratch space anyway and can't be
    relied upon by caller, so it doesn't make a difference if we use them or
    not.

--- include/syscall.h
+++ include/syscall.h
@@ -6,132 +6,32 @@
 #include <stdint.h>
 #include <sys/cdefs.h>
 
-#define __syscall0(num) \
-    ({ \
-        register intptr_t _snum = (intptr_t)(num); \
-        intptr_t _sret; \
-        asm("int $0x80" : "=a"(_sret) : "0"(_snum) : "memory"); \
-        _sret; \
-    })
+static inline intptr_t __syscall6(int num, intptr_t a1, intptr_t a2,
+    intptr_t a3, intptr_t a4, intptr_t a5, intptr_t a6)
+{
+    intptr_t s_ret;
+    intptr_t s_err;
+    register intptr_t snum = (intptr_t)(num);
+    register intptr_t sa1 = (intptr_t)(a1);
+    register intptr_t sa2 = (intptr_t)(a2);
+    register intptr_t sa3 = (intptr_t)(a3);
+    register intptr_t sa4 = (intptr_t)(a4);
+    register intptr_t sa5 __asm__("r8") = (intptr_t)(a5);
+    register intptr_t sa6 __asm__("r9") = (intptr_t)(a6);
+    __asm__ volatile(
+        "int $0x80"
+        : "=a"(s_ret), "=d"(s_err)
+        : "0"(snum), "D"(sa1), "S"(sa2), "d"(sa3), "c"(sa4), "r"(sa5), "r"(sa6)
+        : "memory");
+    return s_ret;
+}
 
-#define __syscall1(num, a1) \
-    ({ \
-        register intptr_t _snum = (intptr_t)(num); \
-        register intptr_t _sa1 = (intptr_t)(a1); \
-        intptr_t _sret; \
-        asm("int $0x80" : "=a"(_sret) : "0"(_snum), "D"(_sa1) : "memory"); \
-        _sret; \
-    })
-
-#define __syscall2(num, a1, a2) \
-    ({ \
-        register intptr_t _snum = (intptr_t)(num); \
-        register intptr_t _sa1 = (intptr_t)(a1); \
-        register intptr_t _sa2 = (intptr_t)(a2); \
-        intptr_t _sret; \
-        asm("int $0x80" \
-            : "=a"(_sret) \
-            : "0"(_snum), "D"(_sa1), "S"(_sa2) \
-            : "memory"); \
-        _sret; \
-    })
-
-#define __syscall3(num, a1, a2, a3) \
-    ({ \
-        register intptr_t _snum = (intptr_t)(num); \
-        register intptr_t _sa1 = (intptr_t)(a1); \
-        register intptr_t _sa2 = (intptr_t)(a2); \
-        register intptr_t _sa3 = (intptr_t)(a3); \
-        intptr_t _sret; \
-        asm("int $0x80" \
-            : "=a"(_sret) \
-            : "0"(_snum), "D"(_sa1), "S"(_sa2), "d"(_sa3) \
-            : "memory"); \
-        _sret; \
-    })
-
-#define __syscall4(num, a1, a2, a3, a4) \
-    ({ \
-        register intptr_t _snum = (intptr_t)(num); \
-        register intptr_t _sa1 = (intptr_t)(a1); \
-        register intptr_t _sa2 = (intptr_t)(a2); \
-        register intptr_t _sa3 = (intptr_t)(a3); \
-        register intptr_t _sa4 = (intptr_t)(a4); \
-        intptr_t _sret; \
-        asm("int $0x80" \
-            : "=a"(_sret) \
-            : "0"(_snum), "D"(_sa1), "S"(_sa2), "d"(_sa3), "c"(_sa4) \
-            : "memory"); \
-        _sret; \
-    })
-
-/*
- * If you must use a specific register, but your Machine Constraints
- * do not provide sufficient control to select the specific register
- * you want, local register variables may provide a solution.
- *
- * [...] using the variable as an input or output operand to the asm
- * guarantees that the specified register is used for that operand.
- */
-
+#define __syscall0(num) __syscall6(num, 0, 0, 0, 0, 0, 0)
+#define __syscall1(num, a1) __syscall6(num, a1, 0, 0, 0, 0, 0)
+#define __syscall2(num, a1, a2) __syscall6(num, a1, a2, 0, 0, 0, 0)
+#define __syscall3(num, a1, a2, a3) __syscall6(num, a1, a2, a3, 0, 0, 0)
+#define __syscall4(num, a1, a2, a3, a4) __syscall6(num, a1, a2, a3, a4, 0, 0)
 #define __syscall5(num, a1, a2, a3, a4, a5) \
-    ({ \
-        register intptr_t _snum = (intptr_t)(num); \
-        register intptr_t _sa1 = (intptr_t)(a1); \
-        register intptr_t _sa2 = (intptr_t)(a2); \
-        register intptr_t _sa3 = (intptr_t)(a3); \
-        register intptr_t _sa4 = (intptr_t)(a4); \
-        register intptr_t _sa5 asm("r8") = (intptr_t)(a5); \
-        intptr_t _sret; \
-        asm("int $0x80" \
-            : "=a"(_sret) \
-            : "0"(_snum), "D"(_sa1), "S"(_sa2), "d"(_sa3), "c"(_sa4), \
-            "r"(_sa5) \
-            : "memory"); \
-        _sret; \
-    })
-
-#define __syscall6(num, a1, a2, a3, a4, a5, a6) \
-    ({ \
-        register intptr_t _snum = (intptr_t)(num); \
-        register intptr_t _sa1 = (intptr_t)(a1); \
-        register intptr_t _sa2 = (intptr_t)(a2); \
-        register intptr_t _sa3 = (intptr_t)(a3); \
-        register intptr_t _sa4 = (intptr_t)(a4); \
-        register intptr_t _sa5 asm("r8") = (intptr_t)(a5); \
-        register intptr_t _sa6 asm("r9") = (intptr_t)(a6); \
-        intptr_t _sret; \
-        asm("int $0x80" \
-            : "=a"(_sret) \
-            : "0"(_snum), "D"(_sa1), "S"(_sa2), "d"(_sa3), "c"(_sa4), \
-            "r"(_sa5), "r"(_sa6) \
-            : "memory"); \
-        _sret; \
-    })
-
-#if 0
-BEGIN_DECLS
-
-intptr_t __syscall0(int syscall_num);
-
-intptr_t __syscall1(int syscall_num, intptr_t arg1);
-
-intptr_t __syscall2(int syscall_num, intptr_t arg1, intptr_t arg2);
-
-intptr_t __syscall3(
-    int syscall_num, intptr_t arg1, intptr_t arg2, intptr_t arg3);
-
-intptr_t __syscall4(int syscall_num, intptr_t arg1, intptr_t arg2,
-    intptr_t arg3, intptr_t arg4);
-
-intptr_t __syscall5(int syscall_num, intptr_t arg1, intptr_t arg2,
-    intptr_t arg3, intptr_t arg4, intptr_t arg5);
-
-intptr_t __syscall6(int syscall_num, intptr_t arg1, intptr_t arg2,
-    intptr_t arg3, intptr_t arg4, intptr_t arg5, intptr_t arg6);
-
-END_DECLS
-
-#endif
+    __syscall6(num, a1, a2, a3, a4, a5, 0)
 
 #endif // _SYSCALL_H_
