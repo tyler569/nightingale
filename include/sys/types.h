7c8cc19e4a197e3ddd040f7cf185a25fb6353e24
Tyler Philbrick <git@choam.me>
2024-04-06 21:29:35 -0700

    types: remove aliases that I don't want to support

--- include/sys/types.h
+++ include/sys/types.h
@@ -8,29 +8,6 @@
 typedef uintptr_t size_t;
 typedef intptr_t ssize_t;
 
-typedef uint8_t u_int8_t;
-typedef uint16_t u_int16_t;
-typedef uint32_t u_int32_t;
-typedef uint64_t u_int64_t;
-
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
-typedef int8_t s8;
-typedef int16_t s16;
-typedef int32_t s32;
-typedef int64_t s64;
-
-typedef unsigned char uchar;
-typedef unsigned short ushort;
-typedef unsigned int uint;
-typedef unsigned long ulong;
-typedef unsigned char u_char;
-typedef unsigned short u_short;
-typedef unsigned int u_int;
-typedef unsigned long u_long;
-
 typedef uintptr_t virt_addr_t;
 typedef uintptr_t phys_addr_t;
 
