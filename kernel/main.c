acbd2d59f4020dcb907868749007ef0a7ebbe319
Tyler Philbrick <git@choam.me>
2025-11-11 17:27:43 -0800

    main: update banner, use #embed, new date

--- kernel/main.c
+++ kernel/main.c
@@ -27,28 +27,9 @@ struct tar_header *initfs;
 int have_fsgsbase = 0;
 bool initialized = false;
 
-const char *banner = "\n\
-********************************\n\
-\n\
-The Nightingale Operating System\n\
-Version " NIGHTINGALE_VERSION "\n\
-\n\
-********************************\n\
-\n\
-Copyright (C) 2017-2024, Tyler Philbrick\n\
-\n\
-This program is free software: you can redistribute it and/or modify\n\
-it under the terms of the GNU General Public License as published by\n\
-the Free Software Foundation, either version 3 of the License, or\n\
-(at your option) any later version.\n\
-\n\
-This program is distributed in the hope that it will be useful,\n\
-but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n\
-GNU General Public License for more details.\n\
-\n\
-You should have received a copy of the GNU General Public License\n\
-along with this program.  If not, see <https://www.gnu.org/licenses/>.\n\n";
+const char banner[] = {
+#embed "banner.txt"
+};
 
 bool print_boot_info = true;
 
@@ -111,9 +92,7 @@ __USED
 		init_program = "/bin/init";
 	bootstrap_usermode(init_program);
 
-	// ext2_info();
-
-	printf("%s", banner);
+	printf("\n%s\n", banner);
 
 	void video();
 	video();
