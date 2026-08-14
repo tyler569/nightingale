99e84165a8fd632363e11346e2de841d93de8189
Tyler Philbrick <git@choam.me>
2026-08-14 13:41:49 -0700

    format: spaces in empty initializer lists
    I don't know why this changed, I haven't changed the formatting scripts,
    but I'm not like upset about it--I do prefer no spaces so I guess we'll
    move forward

--- kernel/print_test.c
+++ kernel/print_test.c
@@ -23,7 +23,7 @@
 	} while (0)
 
 void print_test() {
-	char buffer[256] = { };
+	char buffer[256] = {};
 
 	ASSERT_FMT("Hello, world!", "Hello, %s!", "world");
 	ASSERT_FMT("Hello, 42!", "Hello, %d!", 42);
